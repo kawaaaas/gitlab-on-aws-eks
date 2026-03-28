aa50292f38f529cca3b53c5dd8bfd282437326f6
kawaaaas <sho1314_2423@yahoo.co.jp>
2026-03-29 00:35:40 +0900

    Initial commit

---
+++ test/gitlab-on-aws-eks.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GitlabOnAwsEks from '../lib/gitlab-on-aws-eks-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gitlab-on-aws-eks-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GitlabOnAwsEks.GitlabOnAwsEksStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
